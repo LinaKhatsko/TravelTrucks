8d4569520f06bc103850a26f4b9b6e00369b5c2d
Lina <lina.khatsko@gmail.com>
2026-01-09 19:00:20 +0200

    Initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,30 @@
+import type { Metadata } from "next";
+import { GeistSans } from "geist/font/sans";
+import { GeistMono } from "geist/font/mono";
+import Header from "@/components/Header/Header";
+import Footer from "@/components/Footer/Footer";
+import TanStackProvider from "@/components/TanStackProvider/TanStackProvider";
+import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "NoteHub",
+  description: "Your personal space for notes",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${GeistSans.variable} ${GeistMono.variable}`}>
+        <TanStackProvider>
+          <Header />
+          <main>{children}</main>
+          <Footer />
+        </TanStackProvider>
+      </body>
+    </html>
+  );
+}
