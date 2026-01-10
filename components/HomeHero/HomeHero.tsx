a4a85fb9cb18c3e5f4b06c60edbacf2e3e61ff0d
Lina Khatsko <lina.khatsko@gmail.com>
2026-01-10 20:30:20 +0200

    fix Home_module_css

---
+++ components/HomeHero/HomeHero.tsx
@@ -0,0 +1,20 @@
+import Link from "next/link";
+import styles from "./HomeHero.module.css";
+
+const HomeHero = () => {
+  return (
+      <section className={styles.hero}>
+          <div className={`container ${styles.heroContent}`}>
+              <h1 className={styles.title}>Campers of your dreams</h1>
+              <p className={styles.text}>
+                  You can find everything you want in our catalog
+              </p>
+              <Link href="/catalog" className={styles.button}>
+                  View Now
+              </Link>
+          </div>
+      </section>
+  );
+};
+
+export default HomeHero;
