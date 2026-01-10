53afeed1193a53ca349f5cc66be30639da03850d
Lina Khatsko <lina.khatsko@gmail.com>
2026-01-11 01:05:39 +0200

    fix types add interfaces

--- types/types.ts
+++ types/types.ts
@@ -25,21 +25,68 @@ export interface Camper {
   height: string;
   tank: string;
   consumption: string;
+  gallery: GalleryItem[];
+  reviews: Review[];
+}
 
-  gallery: {
-    thumb: string;
-    original: string;
-  }[];
+export interface CampersQuery {
+  page?: number;
+  limit?: number;
+  location?: string;
+  form?: 'alcove' | 'panelTruck' | 'fullyIntegrated';
+  transmission?: string;
+  AC?: true;
+  bathroom?: true;
+  kitchen?: true;
+  TV?: true;
+  radio?: true;
+  refrigerator?: true;
+  microwave?: true;
+  gas?: true;
+  water?: true;
+}
 
-  reviews: {
+export interface Review {
     reviewer_name: string;
     reviewer_rating: number;
     comment: string;
-  }[];
 }
 
-export interface CampersResponse {
-  total: number;
-  items: Camper[];
+export interface GalleryItem {
+    thumb: string;
+    original: string;
 }
 
+export interface FiltersState {
+  location: string;
+  form: string | null;
+  transmission: string | null;
+  features: {
+    AC: boolean;
+    bathroom: boolean;
+    kitchen: boolean;
+    TV: boolean;
+    radio: boolean;
+    refrigerator: boolean;
+    microwave: boolean;
+    gas: boolean;
+    water: boolean;
+  };
+}
+
+export const initialFilters: FiltersState = {
+  location: "",
+  form: null,
+  transmission: null,
+  features: {
+    AC: false,
+    bathroom: false,
+    kitchen: false,
+    TV: false,
+    radio: false,
+    refrigerator: false,
+    microwave: false,
+    gas: false,
+    water: false,
+  },
+};
