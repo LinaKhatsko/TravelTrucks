393c6dba99458b66f8cebea98e89fff43f47cf5b
Lina Khatsko <lina.khatsko@gmail.com>
2026-01-10 23:55:16 +0200

    сreate types

---
+++ types/types.ts
@@ -0,0 +1,45 @@
+export interface Camper {
+  id: string;
+  name: string;
+  price: number;
+  rating: number;
+  location: string;
+  description: string;
+  form: 'alcove' | 'panelTruck' | 'fullyIntegrated';
+
+  transmission: string;
+  engine: string;
+
+  AC: boolean;
+  bathroom: boolean;
+  kitchen: boolean;
+  TV: boolean;
+  radio: boolean;
+  refrigerator: boolean;
+  microwave: boolean;
+  gas: boolean;
+  water: boolean;
+
+  length: string;
+  width: string;
+  height: string;
+  tank: string;
+  consumption: string;
+
+  gallery: {
+    thumb: string;
+    original: string;
+  }[];
+
+  reviews: {
+    reviewer_name: string;
+    reviewer_rating: number;
+    comment: string;
+  }[];
+}
+
+export interface CampersResponse {
+  total: number;
+  items: Camper[];
+}
+
