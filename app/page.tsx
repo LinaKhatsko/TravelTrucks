327e0c81475e2996ce679e087e43459a5c2b61d3
Lina Khatsko <lina.khatsko@gmail.com>
2026-01-10 20:54:36 +0200

    fix App page

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,14 @@
+import Header from "../components/Header/Header";
 import HomeHero from "../components/HomeHero/HomeHero";
 import css from "./Home.module.css";
 
 export default function HomePage() {
     return (
-        <main className={css.main}>
+        <>
+            <Header />
+            <main className={css.main}>
                 <HomeHero />
-        </main>
+            </main>
+        </>
     );
 }
