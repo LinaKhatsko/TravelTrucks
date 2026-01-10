4961f67556463622c0e1a76cbbc23b8540c72bed
Lina Khatsko <lina.khatsko@gmail.com>
2026-01-10 20:11:58 +0200

    create Header and Header css

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,19 +1,18 @@
+import { Logo } from './Logo';
 import Link from "next/link";
 import css from "./Header.module.css";
 
 const Header = () => {
     return (
-        <header className={css.header}>
-            <Link href="/" aria-label="Home">
-                NoteHub
-            </Link>
+        <header className={`container ${css.header}`}>
+            <Logo className={css.logo}/>
             <nav aria-label="Main Navigation">
                 <ul className={css.navigation}>
                     <li>
                         <Link href="/">Home</Link>
                     </li>
                     <li>
-                        <Link href="/notes">Notes</Link>
+                        <Link href="/notes">Catalog</Link>
                     </li>
                 </ul>
             </nav>
