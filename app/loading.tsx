9ded74e15a275acea6b5ae7b5b8c96adbb003b5b
Lina Khatsko <lina.khatsko@gmail.com>
2026-01-11 23:13:49 +0200

    create loading and css

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,3 +1,9 @@
+import css from "./loading.module.css";
+
 export default function Loading() {
-    return <p>Loading, please wait...</p>;
+    return (
+        <div className={css.container}>
+            <div className={css.loader}></div>
+        </div>
+    );
 }
